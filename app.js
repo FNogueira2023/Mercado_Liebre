ccd50cffd0e7c60238fb06214fad89cf55638c61
FNogueira2023 <f.nogueira2022@outlook.com>
2022-08-27 14:30:36 -0300

    Estructura del register

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
-});
+
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor funcionando');
 });
