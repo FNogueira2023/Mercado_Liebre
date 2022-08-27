f5454e891971610a3dc3064208561136412a26bb
FNogueira2023 <f.nogueira2022@outlook.com>
2022-08-27 17:43:16 -0300

    Estilo listo formularios

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
 
 app.listen(3000, ()=>{
     console.log('Servidor funcionando');
