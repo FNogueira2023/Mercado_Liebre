6c0b608ad937fc2118cf264db613a88aff2c44b7
FNogueira2023 <f.nogueira2022@outlook.com>
2022-08-29 20:13:27 -0300

    Cambios config

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ app.use(express.static('public'));
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
 });
 
-app.get('/register', (req,res)=>{
+app.get('/register', (req, res) => {
     res.sendFile(__dirname + '/views/register.html');
 });
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.sendFile(__dirname + '/views/login.html');
 });
 
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor funcionando');
 });
